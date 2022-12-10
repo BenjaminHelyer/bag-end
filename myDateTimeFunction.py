7fda3338c442dfd82a4f0d2b28c6f0c5a8e212ce
BenjaminHelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2022-12-10 11:51:22 -0600

    Added files in commit

---
+++ myDateTimeFunction.py
@@ -0,0 +1,17 @@
+import os
+import json
+        
+def lambda_handler(event, context):
+    json_region = os.environ['AWS_REGION']
+
+    response = {
+        "statusCode": 200,
+        "updatedResponse": 3,
+        "headers": {
+            "Content-Type": "application/json"
+        },
+        "body": json.dumps({
+            "Region ": json_region
+        })
+    }
+    return response
