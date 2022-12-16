0480d1c18cf0b1bb2ba4b415aff5b1c569ba3e4b
BenjaminHelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2022-12-16 12:06:14 -0600

    Added unit tests for Lambda handler

--- myDateTimeFunction.py
+++ myDateTimeFunction.py
@@ -2,8 +2,25 @@ import os
 import json
         
 def lambda_handler(event, context):
-    json_region = os.environ['AWS_REGION']
+    # put it this way for local testing for now
+    # will adjust later once we figure out a way to do this locally
+    try:
+        json_region = os.environ['AWS_REGION']
+    except:
+        json_region = "us-east-1"
 
+    # I'm not sure how to gracefully handle the case where it's passed an empty event
+    # Maybe it should return an error status code?
+    # Something like the following
+    if event is None:
+        response = ErrorHandler.emptyEventResponse
+        return response
+    else:
+        try:
+            bookName = event["name"]
+        except:
+            bookName = "Les Miserables"
+    
     response = {
         "statusCode": 200,
         "updatedResponse": 3,
@@ -12,6 +29,14 @@ def lambda_handler(event, context):
         },
         "body": json.dumps({
             "Region ": json_region
-        })
+        }),
+        "name": bookName
     }
+
     return response
+
+class ErrorHandler():
+    emptyEventResponse = {
+        "statusCode": 400,
+        "description": "Error: event object passed to Lambda function is empty"
+    }
