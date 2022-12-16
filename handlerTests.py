d425425ecfe2708cfa0a0c1896fef77f61c270b5
BenjaminHelyer <91896754+BenjaminHelyer@users.noreply.github.com>
2022-12-16 14:18:57 -0600

    Added CRUD operations to the Lambda function

--- handlerTests.py
+++ handlerTests.py
@@ -6,11 +6,37 @@ class JsonsForTesting():
     """
     Holds some JSONS that will be used in testing.
     """
-    getRequest = {
-            "http": {
-                "method": "GET"
-            }
+    junkRequest = {
+            "junk": "moreJunk",
+            "hello": "thisIsAllJunk"
         }
+
+    anotherJunkRequest = {
+            "operation": "elephant",
+            "payload": "bear"
+        }
+
+    postRequest = {
+            "operation": "POST",
+            "payload": ""
+        }
+
+    getRequest = {
+            "operation": "GET",
+            "payload": ""
+        }
+
+    putRequest = {
+            "operation": "PUT",
+            "payload": ""
+        }
+
+    deleteRequest = {
+            "operation": "DELETE",
+            "payload": ""
+        }
+
+    
 
 class TestLambda(unittest.TestCase):
     """
@@ -35,6 +61,57 @@ class TestLambda(unittest.TestCase):
 
         # assert whether the tests are equal
         self.assertEqual(result, expectedResult)
+    
+    def test_no_operation(self):
+        """
+        Sees if we catch a case where we have no operation in the event.
+        """
+
+        expectedResult = {
+            "statusCode": 400,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": "{ \"message\": \"Error: unknown operation or payload\" }"
+        }
+
+        result = lambda_handler(JsonsForTesting.junkRequest, None)
+
+        self.assertEqual(result, expectedResult)
+
+    def test_bad_opeartion(self):
+        """
+        Tests if we catch the case where we have a bad operation in the event.
+        """
+
+        expectedResult = {
+            "statusCode": 400,
+                "headers": {
+                    "Content-Type": "application/json"
+                },
+                "body": "{ \"message\": \"Error: unknown operation or payload\" }"
+        }
+
+        result = lambda_handler(JsonsForTesting.anotherJunkRequest, None)
+
+        self.assertEqual(result, expectedResult)
+
+    def test_post_request(self):
+        """
+        Unit test for a POST request from the API.
+        """
+
+        expectedResult = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": "{ \"message\": \"I received a POST Request\" }"
+        }
+
+        result = lambda_handler(JsonsForTesting.postRequest, None)
+
+        self.assertEqual(result, expectedResult)
 
     def test_get_request(self):
         """
@@ -52,6 +129,41 @@ class TestLambda(unittest.TestCase):
         result = lambda_handler(JsonsForTesting.getRequest, None)
 
         self.assertEqual(result, expectedResult)
+
+    def test_put_request(self):
+        """
+        Unit test for a PUT request from the API.
+        """
+
+        expectedResult = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": "{ \"message\": \"I received a PUT Request\" }"
+        }
+
+        result = lambda_handler(JsonsForTesting.putRequest, None)
+
+        self.assertEqual(result, expectedResult)
+
+    def test_delete_request(self):
+        """
+        Unit test for a DELETE request from the API.
+        """
+
+        expectedResult = {
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": "{ \"message\": \"I received a DELETE Request\" }"
+        }
+
+        result = lambda_handler(JsonsForTesting.deleteRequest, None)
+
+        self.assertEqual(result, expectedResult)
+
 
 
 if __name__ == '__main__':
--- processCounter.py
+++ processCounter.py
@@ -2,6 +2,11 @@ import os
 import json
         
 def lambda_handler(event, context):
+    """
+    The event should have the following keys:
+        'operation': operation for interacting with the database
+        'payload': payload for the database
+    """
     # put it this way for local testing for now
     # will adjust later once we figure out a way to do this locally
     try:
@@ -18,17 +23,47 @@ def lambda_handler(event, context):
         "body": "{}"
     }
 
+    # dictionary for functions to perform on a given operation
+    opsFuncs = {
+        'POST' : Db.create,
+        'GET' : Db.read,
+        'PUT' : Db.update,
+        'DELETE': Db.delete
+    }
+
     # I'm not sure how to gracefully handle the case where it's passed an empty event
     # Maybe it should return an error status code?
     # Something like the following
     if event is None:
+        # case where we don't even have an event
         response = ErrorHandler.emptyEventResponse
-        return response
+    elif 'operation' not in event.keys():
+        # case in which we don't even have an operation key in the event
+        response = ErrorHandler.badOpPayload
     else:
-        if event["http"]["method"] == "GET":
-            response["body"] = "{ \"message\": \"I received a GET Request\" }"
+        if event['operation'] in opsFuncs and event['payload'] is not None:
+            # call the relevent function from the Db class with the given payload
+            opsResult = opsFuncs[event['operation']](event['payload'])
+            response["body"] = opsResult
+        else:
+            # send an error response indicating we got a bad operation or payload
+            response = ErrorHandler.badOpPayload
 
     return response
+
+class Db():
+    """
+    Class that holds functions and information for interacting with the database.
+    """
+    def create(payload):
+        return "{ \"message\": \"I received a POST Request\" }"
+    def read(payload):
+        return "{ \"message\": \"I received a GET Request\" }"
+    def update(payload):
+        return "{ \"message\": \"I received a PUT Request\" }"
+    def delete(payload):
+        return "{ \"message\": \"I received a DELETE Request\" }"
+
 
 class ErrorHandler():
     emptyEventResponse = {
@@ -37,4 +72,12 @@ class ErrorHandler():
                 "Content-Type": "application/json"
             },
             "body": "{ \"message\": \"Error: event object passed to Lambda function is empty\" }"
+    }
+
+    badOpPayload = {
+        "statusCode": 400,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": "{ \"message\": \"Error: unknown operation or payload\" }"
     }
